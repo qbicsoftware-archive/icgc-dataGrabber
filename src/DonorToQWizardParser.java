c9d9b7c287e5b241be1f2ff3fde1e0017724eb2c
sven1103 <sven.fillinger@mailbox.org>
2015-11-23 16:17:42 +0100

    Finished Parser, added UML for model overview

---
+++ src/DonorToQWizardParser.java
@@ -0,0 +1,89 @@
+import models.donor.Donor;
+import models.qwizard.AbstractQWizardRow;
+import models.qwizard.QWizardRowFactory;
+import models.qwizard.RowTypes;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by fillinger on 11/23/15.
+ * The allmighty parser class, converting a donor object fetched from ICGC to
+ * the QWizards tsv file format.
+ */
+public class DonorToQWizardParser {
+
+    /**
+     * Parses a list of Donor objects into QWizard tsv file-format.
+     * @param bufferedWriter A buffered writer object
+     * @param donorList a list containing donor objects
+     */
+    public static void parse(BufferedWriter bufferedWriter, List<Donor> donorList, final String SPACE)
+            throws IOException {
+
+        // The barcode production factory
+        QWizardRowFactory qWizardRowFactory = new QWizardRowFactory();
+
+        int donorCounter = 1;
+
+        /*
+        Iterate deeply through the donor object and parse it
+         */
+        for (Donor donor : donorList){
+            // Get a Entity row
+            AbstractQWizardRow entity = qWizardRowFactory.getWizardRow(RowTypes.ENTITY);
+            entity.setEntityNumber(donorCounter);
+            entity.setSecondaryName(donor.getDonorID());
+            entity.setSpace(SPACE);
+            // write row
+            bufferedWriter.write(entity.toString());
+            bufferedWriter.newLine();
+            /*
+            Iterate through specimen
+             */
+            for (Donor.Specimen specimen : donor.getSpecimenList()){
+                // Get a Biological Sample row
+                AbstractQWizardRow bioSample = qWizardRowFactory.getWizardRow(RowTypes.BIO_SAMPLE);
+                bioSample.setEntityNumber();
+                bioSample.setSpace(SPACE);
+                bioSample.setSecondaryName(specimen.getSpecimenID());
+                bioSample.setParent(entity.getEntity());
+                bioSample.setPrimaryTissue(specimen.getSpecimenType());
+                // write row
+                bufferedWriter.write(bioSample.toString());
+                bufferedWriter.newLine();
+                /*
+                Iterate through samples
+                 */
+                for (Donor.Sample sample: specimen.getSampleList()){
+                    // Get the Test Sample and Single Sample Run rows
+                    AbstractQWizardRow testSample = qWizardRowFactory.getWizardRow(RowTypes.TEST_SAMPLE);
+                    AbstractQWizardRow singleSampleRun = qWizardRowFactory.getWizardRow(RowTypes.SINGLE_SAMPLE_RUN);
+                    // Set the content for test sample
+                    testSample.setEntityNumber();
+                    testSample.setSpace(SPACE);
+                    testSample.setSecondaryName(sample.getSampleID());
+                    testSample.setParent(bioSample.getEntity());
+                    testSample.setQSampleType(sample.getLibraryTypes());
+                    // Set the content for single sample run
+                    singleSampleRun.setEntityNumber();
+                    singleSampleRun.setSpace(SPACE);
+                    singleSampleRun.setSecondaryName(sample.getAnalysedID());
+                    singleSampleRun.setParent(testSample.getEntity());
+
+                    // write both rows
+                    bufferedWriter.write(testSample.toString());
+                    bufferedWriter.newLine();
+                    bufferedWriter.write(testSample.toString());
+                    bufferedWriter.newLine();
+
+                }
+
+            }
+            donorCounter++;
+        }
+    }
+
+
+}
--- src/IcgcDataGrabber.java
+++ src/IcgcDataGrabber.java
@@ -1,15 +1,16 @@
-
-import com.sun.rowset.internal.Row;
 import controllers.IcgcDataBaseController;
 
 import models.IcgcDataBaseModel;
 import models.IcgcDonorModel;
 import models.donor.Donor;
-import models.qwizard.AbstractQWizardRow;
-import models.qwizard.QWizardRowFactory;
-import models.qwizard.RowTypes;
+
+import org.apache.commons.cli.*;
 import views.IcgcGrabberView;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,87 +22,87 @@ public class IcgcDataGrabber {
 
     public static void main(String[] args) {
 
+        final File filename;
+
+        Options options = new Options();
+        options.addOption("h", "help", false, "show this help page");
+        options.addOption("o", "output", true, "Writes the ouput in Qwizard readable format.");
+
+        HelpFormatter helpFormatter = new HelpFormatter();
+
+        CommandLineParser parser = new DefaultParser();
+
+        if (args.length == 0){
+            System.err.println("You have to pride a path for the output file!");
+            helpFormatter.printHelp("IcgcDataGrabber.jar -o <FILE> [-h]", options);
+            System.exit(0);
+        }
+
+        try{
+            CommandLine cmd = parser.parse(options, args);
+            if (cmd.hasOption('h')){
+                helpFormatter.printHelp("IcgcDataGrabber.jar -o <FILE> [-h]", options);
+                System.exit(0);
+            }
+            if (cmd.hasOption('o')){
+                filename = new File(cmd.getOptionValue("o") + File.separator + "icgc_parsed.tsv");
+                init(filename);
+            }
+        } catch (ParseException e){
+            System.err.println("Something went wrong reading your command line arguments!\n");
+            helpFormatter.printHelp("IcgcDataGrabber.jar -o <FILE> [-h]", options);
+            System.exit(1);
+        }
+
+
+
+    }
+
+    private static void init(File filename){
         IcgcDataBaseModel model = new IcgcDataBaseModel();
         IcgcGrabberView view = new IcgcGrabberView();
         IcgcDonorModel donorModel = new IcgcDonorModel();
         IcgcDataBaseController controller = new IcgcDataBaseController(model, view, donorModel);
 
-        // Get the information from the ICGC project as JSON object
-        view.printOnConsole("Getting Donor information from project...");
-        controller.makeGetRequest("https://dcc.icgc.org:443/api/v1/projects/OV-AU/donors?size=10000");
-
-        // Extract the donorIDs
-        List<String> donorList = controller.donorIDsfromProject();
-        List<Donor> donorModelList = new ArrayList<>();
-        view.printOnConsole("Getting specimen information for each donor...");
         /*
-        Query the information for every donor
+        Fetch problems with the ouput file before the whole algorithm starts
          */
-        for (String donor : donorList){
-            controller.makeGetRequest("https://dcc.icgc.org:443/api/v1/donors/" + donor + "?include=specimen");
-            donorModelList.add(controller.extractSpecimenInfofromJson());
-        }
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filename))){
+            // Get the information from the ICGC project as JSON object
+            view.printOnConsole("Getting Donor information from project...");
+            controller.makeGetRequest("https://dcc.icgc.org:443/api/v1/projects/OV-AU/donors?size=10000");
 
+            // Extract the donorIDs
+            List<String> donorList = controller.donorIDsfromProject();
+            List<Donor> donorModelList = new ArrayList<>();
+            view.printOnConsole("Getting specimen information for each donor...");
 
-        try{
-            controller.getOutputStream().flush();
-        } catch (Exception e){
-            System.err.println(e.getStackTrace());
-        }
-
-
-        /*
-        Iterate over every Donor object and parse it to QWizard style :)
-         */
-        List<AbstractQWizardRow> qWizardRowList = new ArrayList<>();
-        QWizardRowFactory qWizardRowFactory = new QWizardRowFactory();
-        int donorCounter = 1;
-        final String SPACE = "ICGC_CANCER_SPACE";
-        for (Donor donor : donorModelList){
-            AbstractQWizardRow entity = qWizardRowFactory.getWizardRow(RowTypes.ENTITY);
-            entity.setEntityNumber(donorCounter);
-            entity.setSecondaryName(removeQuotes(donor.getDonorID()));
-            entity.setSpace(SPACE);
-            System.out.println(entity);
-            for (Donor.Specimen specimen : donor.getSpecimenList()){
-                AbstractQWizardRow bioSample = qWizardRowFactory.getWizardRow(RowTypes.BIO_SAMPLE);
-                bioSample.setEntityNumber();
-                bioSample.setSpace(SPACE);
-                bioSample.setSecondaryName(removeQuotes(specimen.getSpecimenID()));
-                bioSample.setParent(entity.getEntity());
-                bioSample.setPrimaryTissue(specimen.getSpecimenType());
-                System.out.println(bioSample);
-                for (Donor.Sample sample: specimen.getSampleList()){
-                    AbstractQWizardRow testSample = qWizardRowFactory.getWizardRow(RowTypes.TEST_SAMPLE);
-                    AbstractQWizardRow singleSampleRun = qWizardRowFactory.getWizardRow(RowTypes.SINGLE_SAMPLE_RUN);
-                    testSample.setEntityNumber();
-                    testSample.setSpace(SPACE);
-                    testSample.setSecondaryName(removeQuotes(sample.getSampleID()));
-                    testSample.setParent(bioSample.getEntity());
-                    testSample.setQSampleType(sample.getLibraryTypes());
-                    System.out.println(testSample);
-                    singleSampleRun.setEntityNumber();
-                    singleSampleRun.setSpace(SPACE);
-                    singleSampleRun.setSecondaryName(sample.getAnalysedID());
-                    singleSampleRun.setParent(testSample.getEntity());
-                    System.out.println(singleSampleRun);
-
-                }
-
+            /*
+            Query the information for every donor
+             */
+            for (String donor : donorList){
+                controller.makeGetRequest("https://dcc.icgc.org:443/api/v1/donors/" + donor + "?include=specimen");
+                donorModelList.add(controller.extractSpecimenInfofromJson());
             }
-            donorCounter++;
+
+            try{
+                controller.getOutputStream().flush();
+            } catch (Exception e){
+                System.err.println(e.getStackTrace());
+            }
+
+            /*
+            Parse the shit
+             */
+            DonorToQWizardParser.parse(bufferedWriter, donorModelList, "");
+
+        } catch (IOException e){
+            System.err.println("Could not open/find file: " + filename);
+            System.err.println(e);
+            System.exit(1);
         }
-
     }
 
-    /**
-     * Remove quotes from Strings, as they may occur in JSON fields
-     * @param string
-     * @return
-     */
-    public static String removeQuotes(String string){
-        return string.replaceAll("\"", "");
-    }
 }
 
 
